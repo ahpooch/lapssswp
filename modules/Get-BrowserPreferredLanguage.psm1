c98b44f29a5b9c67427098ce752f0dee328a2d1f
ahpooch <ahpooch@hotmail.com>
2025-02-10 13:03:24 +0300

    Get-BrowserPreferredLanguage fixed
    Fixed problem cousing http 500 error.
    Added different locale variations for en and ru.

--- modules/Get-BrowserPreferredLanguage.psm1
+++ modules/Get-BrowserPreferredLanguage.psm1
@@ -1,14 +1,17 @@
 function Get-BrowserPreferredLanguage {
     # Getting default portal language
-    $browser_Preferred_language = (Get-PodeConfig).Language
+    $browser_Preferred_language = (Get-PodeConfig).DefaultLanguage
     # Getting user preferred language from Accept-Language Header
     $accept_language_header = $(Get-PodeHeader -Name 'Accept-Language') #ru,en;q=0.9,en-US;q=0.8
     
     # Setting language
     if ((Get-PodeConfig).UseBrowserPreferredLanguage){
         # Choosing from localizations available in ..\localization.json file.
-        if ($accept_language_header.split(",")[0] -eq "en") { $browser_Preferred_language = "en" }
-        if ($accept_language_header.split(",")[0] -eq "ru") { $browser_Preferred_language = "ru" }
+        if ($null -ne $accept_language_header){
+            if ("en-US", "en-GB", "en" -contains $accept_language_header.split(",")[0]) { $browser_Preferred_language = "en" }
+            if ("ru-RU", "ru" -contains $accept_language_header.split(",")[0]) { $browser_Preferred_language = "ru" }
+        }
+
     }
     
     return $browser_Preferred_language
